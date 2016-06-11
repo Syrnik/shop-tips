bcb0be97206209bad1fc86a05a645668e53f3049
Serge Rodovnichenko <sergerod@gmail.com>
2016-06-11 14:40:55 +0300

    Docblock added to config file

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Tips plugin for Shop-Script 5+
+ *
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version 1.1.0
+ * @copyright Serge Rodovnichenko, 2015-2016
+ * @license MIT
+ */
 return array(
     'name'     => 'Полезные мелочи',
     'img'     => 'img/tips.png',
