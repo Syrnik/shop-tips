e1b6e62c91b967a12a325cc245f8659c4627613e
Serge Rodovnichenko <sergerod@gmail.com>
2016-10-06 12:49:13 +0300

    Добавил поддержку хука backend_order

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -9,11 +9,12 @@
  */
 return array(
     'name'     => 'Полезные мелочи',
-    'img'     => 'img/tips.png',
+    'img'      => 'img/tips.png',
     'version'  => '1.1.0',
     'vendor'   => '670917',
     'handlers' =>
         array(
-            'backend_product' => 'hookBackendProduct'
+            'backend_product' => 'hookBackendProduct',
+            'backend_order'   => 'hookBackendOrder'
         ),
 );
