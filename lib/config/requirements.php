bdbcf6122cfb4c13673e89277c904f547abfcad1
Serge Rodovnichenko <sergerod@gmail.com>
2023-02-26 10:46:31 +0300

    Доработаны системные требования

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -1,6 +1,5 @@
 <?php
-return array(
-    'app.installer' => array(
-        'version' => 'latest', 'strict' => true,
-    ),
-);
+return [
+    'app.installer' => ['version' => '>=2.0.0', 'strict' => true],
+    'php'           => ['version' => '>=7.2.0', 'strict' => true]
+];
